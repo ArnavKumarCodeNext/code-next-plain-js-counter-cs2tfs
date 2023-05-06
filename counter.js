6f72a0f954725664a63e30027718c78e485884be
ArnavKumarCodeNext <111657877+ArnavKumarCodeNext@users.noreply.github.com>
2023-05-06 12:10:31 -0400

    2 files modified

--- counter.js
+++ counter.js
@@ -1,1 +1,17 @@
 // TODO: Add counter code here.
+const countSpan = document.getElementById('count');
+const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
+let count = 10;
+console.log(count);
+console.log(incrementButton);
+console.log(decrementButton);
+
+
+countSpan.innerText = count;
+
+decrementButton.addEventListener('click',()=>{
+
+
+
+})
